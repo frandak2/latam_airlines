0cf5b3542ed44c26fa760ad33fa37cc51dab984f
frandak2 <frank.montenegro@correounivalle.edu.co>
2023-02-15 14:50:36 -0500

    adding testing

--- api/app/models.py
+++ api/app/models.py
@@ -1,19 +1,38 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class PredictionRequest(BaseModel):
-    DIA: int
-    MES : int
-    HORA: int
-    MIN: int
-    Vlo_change: int
-    Emp_change: int
-    temporada_alta: int
-    periodo_dia: str
-    DIANOM: str
-    MESNOM: str
-    Des_I: str
-    TIPOVUELO: str
-    OPERA: str
+    DIA: int = Field(default=1, ge=1, le=31)
+    MES: int = Field(default=1, ge=1, le=12)
+    HORA: int = Field(default=0, ge=0, le=23)
+    MIN: int = Field(default=15, ge=0, le=59)
+    Vlo_change: int = Field(default=0, ge=0, le=1)
+    Emp_change: int = Field(default=0, ge=0, le=1)
+    temporada_alta: int = Field(default=1, ge=0, le=1)
+    periodo_dia: str = Field(min_length=5, max_length=6)
+    DIANOM: str = Field(min_length=5, max_length=9)
+    MESNOM: str = Field(min_length=3)
+    Des_I: str = Field(min_length=3, max_length=5)
+    TIPOVUELO: str = Field(min_length=1)
+    OPERA: str = Field(min_length=3, max_length=15)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "DIA": 1,
+                "MES": 1,
+                "HORA": 0,
+                "MIN": 15,
+                "Vlo_change": 0,
+                "Emp_change": 0,
+                "temporada_alta": 1,
+                "periodo_dia": "noche",
+                "DIANOM": "Domingo",
+                "MESNOM": "Ene",
+                "Des_I": "MMMX",
+                "TIPOVUELO": "I",
+                "OPERA": "Aeromexico"
+            }
+        }
 
 class PredictionResponse(BaseModel):
     atraso_15: int
